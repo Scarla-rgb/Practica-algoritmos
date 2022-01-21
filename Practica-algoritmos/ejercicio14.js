3f6060e0215daa10fcec5c2e7b92ba390dcf6d3f
Scarla Rodríguez <79226768+Scarla-rgb@users.noreply.github.com>
2022-01-21 16:30:18 -0400

    ejercicio14

---
+++ Practica-algoritmos/ejercicio14.js
@@ -0,0 +1,21 @@
+var max = Math.max(...arr);
+
+var new_arr = []; 
+for (let x of arr) {
+    new_arr.push (parseInt ((x * 10) / max));
+}   
+
+for (let i of new_arr){
+    var contador = i;
+    document.write (ciclo (contador) + "<br>");
+}
+
+//Funcion:
+function ciclo (contador){
+    let car = "+";
+    let car2 = car;
+    for (let j = 1; j < contador; j++) {
+        car = car + car2;  
+    } 
+    return car
+}
